9bdfc01a64a48e5ad635519543bec203d33fe08a
Universal-AD1 <77748829+Universal-AD1@users.noreply.github.com>
2022-10-17 02:39:50 -0400

    Added bootstrap to components, added links to all pages,

---
+++ src/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Footer = () => {
+    return ( 
+        <footer className = "d-block" >
+        <p className = "text-center" > ©Copyright 2021 Lead Capture Page </p>
+        </footer>
+    );
+};
+export default Footer;
--- src/navigationBar.js
+++ src/navigationBar.js
@@ -1,34 +1,52 @@
+
 import { Link } from 'react-router-dom';
+
+
 
 export function NavigationBar() {
     return (
-        <nav class="navbar background">
-            <ul class="nav-list">
-                <div class="logo">
-                    <img src={require("./assets/rocket.png")} alt='Astro-Learn Rocket'></img>
-                </div>
-                <Link to="/register"><li>Register</li></Link>
-                <Link to="/login"><li>Login</li></Link>
-                <li><a href="#tutorial">Tutorial</a></li>
-                <li><a href="#quizzes">Quizzes</a></li>
-                <li><a href='#about'>About us</a></li>
-            </ul>
-            <div class="rightNav">
-                <ul class="nav-list">
-                    <li><a href='#profile'>Profile</a></li>
-                    <Link to="/register">Register</Link>
-                    <Link to="/login">Login</Link>
+    <nav class="navbar navbar-expand-lg navbar-dark  bg-dark">
+        <div class="container-fluid">
+        <Link style={{color: 'white',textDecoration: 'none' }} to="/"><a class="navbar-brand text-white" >
+                 <img src={require("./assets/rocket.png")} alt='Astro-Learn Rocket' width="50" height="50" ></img>
+                Astro-Learn
+            </a></Link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active " aria-current="page"><Link style={{color: 'white',textDecoration: 'none' }} to="/register"><li>Register</li></Link></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"><Link style={{color: 'white',textDecoration: 'none' }} to="/login">Login</Link></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"><Link style={{color: 'white',textDecoration: 'none' }} to="/category">Categories</Link></a>
+                    </li>
+                    
+            
                 </ul>
+                
             </div>
-            {/*
-					 Search bar here if we want it later
-					<div class="rightNav">
-					<input type="text" name="search" id="search" />
-					<button class="btn btn-sm">Search</button>
-				</div>
-					*/}
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                        <a class="nav-link" aria-current="page"><Link style={{color: 'white',textDecoration: 'none' }} to="/register"><li>Register</li></Link></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"><Link style={{color: 'white',textDecoration: 'none' }} to="/login">Login</Link></a>
+                    </li>
+            </ul>
 
+            {/*
+            <form class="d-flex justify-content-end">
+                <input class="form-control mx-2" type="search" placeholder="Search" aria-label="Search"></input>
+                <button class="btn btn-outline-light" type="submit">Search</button>
+            </form> */}
+        </div>
+        
         </nav>
-    )
+)
 }
 export default NavigationBar
