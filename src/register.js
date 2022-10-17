90dbf632e901fca54eb8a1b48104ac92486bc273
tejsingh8 <52592952+tejsingh8@users.noreply.github.com>
2022-10-16 20:23:38 -0400

    create style sheets for login and register pages add divs and styling for login and register js pages

--- src/register.js
+++ src/register.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-
+import './register.css';
 const Register = () => {
     const [userName, setUserName] = useState('')
     const [password, setPassword] = useState('')
@@ -9,14 +9,28 @@ const Register = () => {
     console.log(userName, password)
 
     return (
-        <form onSubmit={userRegistration}>
-            <h2>Sign Up</h2>
-            <label>Username</label>
-            <input type="userName" onChange={(e) => setUserName(e.target.value)} value={userName} />
-            <label>Password</label>
-            <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
-            <button>Register</button>
-        </form>
+        <>
+            <div class="outside_register_div">
+                <div class="inside_register_div">
+                    <form class="register_form" onSubmit={userRegistration}>
+                        <h2 class="register_heading2">Sign Up</h2>
+                        <div class="register_form_div">
+                            {/* <label>Username</label> */}
+                            <input class="register_input" placeholder="Username" type="userName" onChange={(e) => setUserName(e.target.value)} value={userName} />
+                            <br />
+                            <input class="register_input" placeholder="School" type="school" onChange={(e) => setUserName(e.target.value)} value={userName} />
+                            <br />
+                            {/* <label >Password</label> */}
+                            <input class="register_input" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
+                                           <br />
+                                           <input class="register_input" type="confirmPassword" placeholder="Confirm Password" onChange={(e) => setPassword(e.target.value)} value={password} />
+                                           <br />
+                            <button class="login_button">Register</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>        
     )
 }
 export default Register
