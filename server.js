c8a3407f2751ea0dd0e577dc7953750d7a30d825
ParkerGoby <77749544+ParkerGoby@users.noreply.github.com>
2022-11-14 16:17:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
+const path = require("path");
+app.use(express.static(path.resolve(__dirname, "./build")));
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
 app.use(require("./routes/login"));
 // get driver connection
 const dbo = require("./db/conn");
+const { response } = require("express");
+
  
 app.listen(port, () => {
   // perform a database connection when server starts
