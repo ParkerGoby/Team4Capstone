3e9f2a9b695d37cec75870c6df506258b31fbea7
tejsingh8 <52592952+tejsingh8@users.noreply.github.com>
2022-11-19 18:31:46 -0500

    Update level1guidebook.js

--- src/guidebook/addition/level1guidebook.js
+++ src/guidebook/addition/level1guidebook.js
@@ -14,7 +14,8 @@ const Level1GuideAddition = () => {
                 </p>
                 
             </div> */}
-            <article class="mb-4">
+            <br></br>
+            <article class="mb-4" >
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-md-10 col-lg-8 col-xl-7">
