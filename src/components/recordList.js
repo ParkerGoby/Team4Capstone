34759ea1100b86dfd7acb6955c29bfad2563399b
tejsingh8 <52592952+tejsingh8@users.noreply.github.com>
2022-11-18 21:33:22 -0500

    update localhost to heroku

--- src/components/recordList.js
+++ src/components/recordList.js
@@ -25,7 +25,7 @@ export default function RecordList() {
   // This method fetches the records from the database.
   useEffect(() => {
     async function getRecords() {
-      const response = await fetch(`http://localhost:5000/login/`);
+      const response = await fetch(`https://astro-learn.herokuapp.com/login/`);
 
       if (!response.ok) {
         const message = `An error occured: ${response.statusText}`;
@@ -44,7 +44,7 @@ export default function RecordList() {
 
   // This method will delete a record
   async function deleteRecord(id) {
-    await fetch(`http://localhost:5000/${id}`, {
+    await fetch(`https://astro-learn.herokuapp.com/${id}`, {
       method: "DELETE"
     });
 
