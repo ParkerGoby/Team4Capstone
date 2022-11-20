a37d9b2b12af19aefe71b4ed52cedb5a7d385ae2
jayfrankina <jayfrankina@oakland.edu>
2022-11-19 20:07:38 -0500

    TestingWQuestions
    Adding Question Info for Dynamically Pulling Question Info For User To Select From

--- src/question1.js
+++ src/question1.js
@@ -1,40 +1,83 @@
-// import { useState } from "react"
+import { useState } from "react"
 import './login.css';
-const Login = () => {
-    // const [userName, setUserName] = useState('')
-    // const [password, setPassword] = useState('')
 
-    // const userLogin = async (e) =>
-    //     e.preventDefault()
-    // console.log(userName, password)
+ export function QuestionList() {
+let questions = [
+  {
+    id: 1,
+    question: "What is 2+2?",
+    answer: "4",
+    options: [
+      "3", "2", "5", "4",
+    ],
+  },
+  {
+    id: 2,
+    question: "What is 3+7?",
+    answer: "10",
+    options: [
+      "8", "10", "14", "12", 
+      ],
+  },
+  {
+    id: 3,
+    question: "What is 12+17",
+    answer: "29",
+    options: [
+      "31", "30", "29", "39",
+    ],
+  },
+  {
+    id: 4,
+    question: "What is 27 + 36",
+    answer: "63",
+    options: [
+      "63", "73", "61", "53",
+    ],
+  },
+  {
+    id: 5,
+    question: "What is 65 + 29",
+    answer: "94",
+    options: [
+      "95", "84", "90", "94",
+    ],
+  },
+];
 
-// var employeeInd = employees.find({email: "testingWDB"});
-// var employees;
-// while (employeeInd.hasNext()) {
-//     employees = employeeInd.next();
-//     console.log(employees.email);
-// }
+function randomizeQuestions(){
+
+}
+
+const [index, setQuestion] = useState(0);
+
+const answerButtonListener = (correctAnswerSel) => {
+  const nextQ = index + 1;
+  if (nextQ < questions.length) {
+    setQuestion(nextQ)
+  }
+  else {
+    return ("Incorrect. Try again.")
+  }
+}
     return (
-        <>
-         
-        <>
-
+        <>        
             <div class="title-container">        
             <div class="container-fluid">
             
                 <div class="row">
-                <div class="col-xs-1 text-center"><h1>What is 2 + 2 = ?</h1></div>
+                <div class="col-xs-1 text-center question"><h1>
+                  {questions[index].question}</h1></div>
                 </div>
             </div>
             </div>
         
-        <div class="container-thing">
+        <div class="container-thing questionOptions">
             <div class="row row-cols-1 row-cols-md-2 g-4">
-                
                 <div class="col">
                     <div class="btn btn-outline-dark card">
                     <div class="card-body">
-                        <h5 class="card-title text-center align-middle">123</h5>
+                        <h5 class="card-title text-center align-middle">{questions[index].options[0]}</h5>
                         { /*<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>*/}
                         { /* <p>Addition</p> */ }
                     </div>
@@ -43,7 +86,7 @@ const Login = () => {
                 <div class="col">
                     <div class="btn btn-outline-dark card">
                     <div class="card-body">
-                        <h5 class="card-title text-center align-middle">345</h5>
+                        <h5 class="card-title text-center align-middle">{questions[index].options[1]}</h5>
                         { /*<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>*/}
                         { /* <p>Addition</p> */ }
                     </div>
@@ -52,7 +95,7 @@ const Login = () => {
                 <div class="col">
                     <div class="btn btn-outline-dark card">
                     <div class="card-body">
-                        <h5 class="card-title text-center align-middle">3</h5>
+                        <h5 class="card-title text-center align-middle">{questions[index].options[2]}</h5>
                         { /* <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> */}
                         { /* <p>Addition</p> */ }
                     </div>
@@ -61,7 +104,7 @@ const Login = () => {
                 <div class="col">
                     <div class="btn btn-outline-dark card">
                     <div class="card-body">
-                        <h5 class="card-title text-center align-middle">4</h5>
+                        <h5 class="card-title text-center align-middle">{questions[index].options[3]}</h5>
                         { /*<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>*/}
                        { /* <p>Addition</p> */ }
                     </div>
@@ -70,8 +113,9 @@ const Login = () => {
             </div>
             </div>
             </>
-</>
     )
 }
 
-export default Login
+    
+
+export default QuestionList
