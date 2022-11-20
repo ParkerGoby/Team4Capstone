afc28e3f9c534b0f480a72604a8b98e0edb49b4c
tejsingh8 <52592952+tejsingh8@users.noreply.github.com>
2022-11-19 21:23:56 -0500

    automatic question and answer generator for addition

--- src/question1.js
+++ src/question1.js
@@ -1,40 +1,60 @@
-// import { useState } from "react"
 import './login.css';
-const Login = () => {
-    // const [userName, setUserName] = useState('')
-    // const [password, setPassword] = useState('')
 
-    // const userLogin = async (e) =>
-    //     e.preventDefault()
-    // console.log(userName, password)
+ export function QuestionList() {
+  var x = Math.floor(Math.random() * 10); //0 and 1
+  var y = Math.floor(Math.random() * 10);
 
-// var employeeInd = employees.find({email: "testingWDB"});
-// var employees;
-// while (employeeInd.hasNext()) {
-//     employees = employeeInd.next();
-//     console.log(employees.email);
-// }
+  var sum = x + y;
+  var opt1 = sum + Math.floor(Math.random() * 10) + 1;
+  var opt2 = sum + Math.floor(Math.random() * 10) + 4;
+  var opt3 = sum + Math.floor(Math.random() * 10) + 7;
+
+ // const answers = [opt1, opt2, opt3, sum];
+/////////////////////////////////////////////////////////////////////////
+                  function shuffle(array) {
+                    let currentIndex = array.length,  randomIndex;
+
+                    // While there remain elements to shuffle.
+                    while (currentIndex !== 0) {
+
+                      // Pick a remaining element.
+                      randomIndex = Math.floor(Math.random() * currentIndex);
+                      currentIndex--;
+
+                      // And swap it with the current element.
+                      [array[currentIndex], array[randomIndex]] = [
+                        array[randomIndex], array[currentIndex]];
+                    }
+
+                    return array;
+                  }
+
+                  // Used like so
+                  var answers = [opt1, opt2, opt3, sum];
+                  shuffle(answers);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// const [index, setQuestion] = useState(0);
+
     return (
-        <>
-         
-        <>
-
+        <>        
             <div class="title-container">        
             <div class="container-fluid">
             
                 <div class="row">
-                <div class="col-xs-1 text-center"><h1>What is 2 + 2 = ?</h1></div>
+                <div class="col-xs-1 text-center question"><h1>What is {x} + {y}?</h1></div>
                 </div>
             </div>
             </div>
         
-        <div class="container-thing">
+        <div class="container-thing questionOptions">
             <div class="row row-cols-1 row-cols-md-2 g-4">
-                
                 <div class="col">
                     <div class="btn btn-outline-dark card">
                     <div class="card-body">
-                        <h5 class="card-title text-center align-middle">123</h5>
+                        <h5 class="card-title text-center align-middle"><h2>{answers[0]}</h2></h5>
                         { /*<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>*/}
                         { /* <p>Addition</p> */ }
                     </div>
@@ -43,7 +63,7 @@ const Login = () => {
                 <div class="col">
                     <div class="btn btn-outline-dark card">
                     <div class="card-body">
-                        <h5 class="card-title text-center align-middle">345</h5>
+                        <h5 class="card-title text-center align-middle"><h2>{answers[1]}</h2></h5>
                         { /*<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>*/}
                         { /* <p>Addition</p> */ }
                     </div>
@@ -52,7 +72,7 @@ const Login = () => {
                 <div class="col">
                     <div class="btn btn-outline-dark card">
                     <div class="card-body">
-                        <h5 class="card-title text-center align-middle">3</h5>
+                        <h5 class="card-title text-center align-middle"><h2>{answers[2]}</h2></h5>
                         { /* <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> */}
                         { /* <p>Addition</p> */ }
                     </div>
@@ -61,7 +81,7 @@ const Login = () => {
                 <div class="col">
                     <div class="btn btn-outline-dark card">
                     <div class="card-body">
-                        <h5 class="card-title text-center align-middle">4</h5>
+                        <h5 class="card-title text-center align-middle"><h2>{answers[3]}</h2></h5>
                         { /*<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>*/}
                        { /* <p>Addition</p> */ }
                     </div>
@@ -70,8 +90,9 @@ const Login = () => {
             </div>
             </div>
             </>
-</>
     )
 }
 
-export default Login
+    
+
+export default QuestionList
