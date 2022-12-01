2854f4ed01839419f00aa00af4c74bafca0c77e0
ParkerGoby <77749544+ParkerGoby@users.noreply.github.com>
2022-12-01 18:21:35 -0500

    Update navigationBar.js

--- src/navigationBar.js
+++ src/navigationBar.js
@@ -1,6 +1,43 @@
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import jwt_decode from "jwt-decode";
+import {useState} from 'react'
+
 
 export function NavigationBar() {
+  const [ user, setUser ] = useState({});
+
+  function handleCallbackResponse(response) {
+      console.log("Encoded JWT ID token: " + response.credential);
+      var userObject = jwt_decode(response.credential);
+      console.log(userObject);
+      setUser(userObject);
+      document.getElementById("signInDiv").hidden = true;
+  }
+
+  function handleSignOut(event) {
+      setUser({});
+      document.getElementById("signInDiv").hidden = false
+  }
+
+
+  useEffect(() => {
+      /* global google */
+      google.accounts.id.initialize({
+          client_id: "755566697486-amgo7bd3lmn1s3i2bfvepjunhjro8e2v.apps.googleusercontent.com",
+          callback: handleCallbackResponse
+      });
+
+      google.accounts.id.renderButton(
+          document.getElementById("signInDiv"),
+          { theme: "outline", size: "large"}
+      );
+
+      google.accounts.id.prompt();
+  }, []);
+
+
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark  bg-dark">
       <div className="container-fluid">
@@ -61,6 +98,28 @@ export function NavigationBar() {
               </Link>
             </a>
           </li>
+          <>
+        <div className="App">
+
+        <div className="container">
+    <div className="row">
+      <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+      <div id="signInDiv"></div>
+
+        {
+
+            Object.keys(user).length != 0 &&
+
+            <button onClick={(e) => handleSignOut(e)}> sign out</button>
+
+        }
+
+            </div>
+      </div>
+    </div>
+  </div>
+      
+    </>
           <li className="nav-item">
             <a className="nav-link">
               <Link
