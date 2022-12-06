9c5e3a3398169e304f2d6f29ec50326aaed5a029
jayfrankina <jayfrankina@oakland.edu>
2022-12-06 17:36:00 -0500

    logout/TestTrophiesAdded

--- src/navigationBar.js
+++ src/navigationBar.js
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router";
 
 
+var token;
 
 export function NavigationBar() {
+    const navigate = useNavigate();
     if (sessionStorage.getItem("token") === null) {
         
       
@@ -47,47 +50,42 @@ export function NavigationBar() {
 }
 
 else{
+    function logOut() {
+        sessionStorage.clear(token)
+        window.location.reload();
+        navigate("/");
+    }    
 return (
     <nav className="navbar navbar-expand-lg navbar-dark  bg-dark">
         <div className="container-fluid">
-        <Link style={{color: 'white',textDecoration: 'none' }} to="/"><a className="navbar-brand text-white" >
+        <Link className="navbar-brand text-white" style={{color: 'white',textDecoration: 'none' }} to="/">
                  <img src={require("./assets/rocket.png")} alt='Astro-Learn Rocket' width="50" height="50" ></img>
                 Astro-Learn
-            </a></Link>
+            </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
-
                     <li className="nav-item">
-                        <a className="nav-link"><Link style={{color: 'white',textDecoration: 'none' }} to="/category">Categories</Link></a>
+                        <Link className='nav-link' style={{color: 'white',textDecoration: 'none' }} to="/category">Categories</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link"><Link style={{color: 'white',textDecoration: 'none' }} to="/guidebook">Guidebooks</Link></a>
+                        <Link className='nav-link' style={{color: 'white',textDecoration: 'none' }} to="/guidebook/guidebook">Guidebooks</Link>
                     </li>
                 </ul>
-                
             </div>
-            
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <a className="nav-link"><Link style={{color: 'white',textDecoration: 'none' }} to="/profile">Profile</Link></a>
+                        <Link className='nav-link' style={{color: 'white',textDecoration: 'none' }} to="/profile">Profile</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link"><Link style={{color: 'white',textDecoration: 'none' }} to="/profile">Logout</Link></a>
+                        <Link className='nav-link' style={{color: 'white',textDecoration: 'none' }} onClick={logOut}>Logout</Link>
                     </li>
             </ul>
-
-
-        </div>
-        
-        </nav>
+        </div>  
+    </nav>
 )
-
-
-
 }}
 
 
