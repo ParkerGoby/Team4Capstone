e94a33e1a264f716532be2e7cef2ca287324a459
jayfrankina <jayfrankina@oakland.edu>
2022-12-07 01:32:09 -0500

    Trophy/Profile Page Front End Affected

--- src/profile.js
+++ src/profile.js
@@ -202,7 +202,7 @@ else{
                     </div>
                   </div>
                 </div>
-                <div className="container-thing">
+                <div className="trophy-container">  
                   <div className="row row-cols-1 row-cols-md-1 g-4">
                     <div className="col ">
                       <Card bg="dark" text="white" className="card my-4">
@@ -218,90 +218,90 @@ else{
                           <h3 className="text-center">Your Trophies</h3>
                           <h3 style={{margin:"3rem"}}className="text-center">Addition Trophies</h3>
                             
-                            <div  className="row row-cols-1 row-cols-md-4 g-4">
-                              <div  style={{display:"none"}} id="additionTrophy1" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture1.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 1</figcaption></figure>
+                            <div  className="row row-cols-1 row-cols-md-3 g-4">
+                              <div  style={{display:"none"}} id="additionTrophy1" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture1.png")} /><figcaption className="text-center">Level 1</figcaption></figure>
                               </div>
-                              <div style={{display:"none"}} id="additionTrophy2" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture2.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 2</figcaption></figure>
+                              <div style={{display:"none"}} id="additionTrophy2" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture2.png")} /><figcaption className="text-center">Level 2</figcaption></figure>
                               </div>
-                              <div  style={{display:"none"}} id="additionTrophy3" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture3.png")} /> <figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 3</figcaption></figure>
+                              <div  style={{display:"none"}} id="additionTrophy3" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture3.png")} /> <figcaption className="text-center">Level 3</figcaption></figure>
                                 
                               </div>
-                              <div  style={{display:"none"}} id="additionTrophy4" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture4.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 4</figcaption></figure>
+                              <div  style={{display:"none"}} id="additionTrophy4" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture4.png")} /><figcaption className="text-center">Level 4</figcaption></figure>
                               </div>
-                              <div  style={{display:"none"}} id="additionTrophy5" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture5.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 5</figcaption></figure>
+                              <div  style={{display:"none"}} id="additionTrophy5" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture5.png")} /><figcaption className="text-center">Level 5</figcaption></figure>
                               </div>
-                              <div  style={{display:"none"}} id="additionTrophy6" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture6.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 6</figcaption></figure>
+                              <div  style={{display:"none"}} id="additionTrophy6" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture6.png")} /><figcaption className="text-center">Level 6</figcaption></figure>
                               </div></div>
 
                               <div className="block">
                               <h3 className="text-center" style={{margin:"3rem"}}>Subtraction Trophies</h3>
-                              <div  className="row row-cols-1 row-cols-md-4 g-4">
-                              <div  style={{display:"none"}} id="subtractionTrophy1" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture7.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 1</figcaption></figure>
+                              <div  className="row row-cols-1 row-cols-md-3 g-4">
+                              <div  style={{display:"none"}} id="subtractionTrophy1" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture7.png")} /><figcaption className="text-center">Level 1</figcaption></figure>
                               </div>
-                              <div style={{display:"none"}} id="subtractionTrophy2" className="col">
-                              <figure><img  style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture8.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 2</figcaption></figure>
+                              <div style={{display:"none"}} id="subtractionTrophy2" className="col text-center">
+                              <figure><img  style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture8.png")} /><figcaption className="text-center">Level 2</figcaption></figure>
                               </div>
-                              <div  style={{display:"none"}} id="subtractionTrophy3" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture9.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 3</figcaption></figure>
+                              <div  style={{display:"none"}} id="subtractionTrophy3" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture9.png")} /><figcaption className="text-center">Level 3</figcaption></figure>
                               </div>
-                              <div  style={{display:"none"}} id="subtractionTrophy4" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture10.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 4</figcaption></figure>
+                              <div  style={{display:"none"}} id="subtractionTrophy4" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture10.png")} /><figcaption className="text-center">Level 4</figcaption></figure>
                               </div>
-                              <div  style={{display:"none"}} id="subtractionTrophy5" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture11.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 5</figcaption></figure>
+                              <div  style={{display:"none"}} id="subtractionTrophy5" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture11.png")} /><figcaption className="text-center">Level 5</figcaption></figure>
                               </div>
-                              <div  style={{display:"none"}} id="subtractionTrophy6" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture12.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 6</figcaption></figure>
+                              <div  style={{display:"none"}} id="subtractionTrophy6" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture12.png")} /><figcaption className="text-center">Level 6</figcaption></figure>
                               </div></div></div>
 
                               <h3 style={{margin:"3rem"}} className="text-center">Multiplication Trophies</h3>
-                              <div  className="row row-cols-1 row-cols-md-4 g-4">
-                              <div  style={{display:"none"}} id="multiplicationTrophy1" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture13.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 1</figcaption></figure>
+                              <div  className="row row-cols-1 row-cols-md-3 g-4">
+                              <div  style={{display:"none"}} id="multiplicationTrophy1" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture13.png")} /><figcaption className="text-center">Level 1</figcaption></figure>
                               </div>
-                              <div style={{display:"none"}} id="multiplicationTrophy2" className="col">
-                              <figure><img  style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture14.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 2</figcaption></figure>
+                              <div style={{display:"none"}} id="multiplicationTrophy2" className="col text-center">
+                              <figure><img  style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture14.png")} /><figcaption className="text-center">Level 2</figcaption></figure>
                               </div>
-                              <div  style={{display:"none"}} id="multiplicationTrophy3" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture15.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 3</figcaption></figure>
+                              <div  style={{display:"none"}} id="multiplicationTrophy3" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture15.png")} /><figcaption className="text-center">Level 3</figcaption></figure>
                               </div>
-                              <div  style={{display:"none"}} id="multiplicationTrophy4" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture18.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 4</figcaption></figure>
+                              <div  style={{display:"none"}} id="multiplicationTrophy4" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture18.png")} /><figcaption className="text-center">Level 4</figcaption></figure>
                               </div>
-                              <div  style={{display:"none"}} id="multiplicationTrophy5" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture19.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 5</figcaption></figure>
+                              <div  style={{display:"none"}} id="multiplicationTrophy5" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture19.png")} /><figcaption className="text-center">Level 5</figcaption></figure>
                               </div>
-                              <div  style={{display:"none"}} id="multiplicationTrophy6" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture20.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 6</figcaption></figure>
+                              <div  style={{display:"none"}} id="multiplicationTrophy6" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture20.png")} /><figcaption className="text-center">Level 6</figcaption></figure>
                               </div></div>
 
                               <h3 style={{margin:"3rem"}} className="text-center">Division Trophies</h3>
-                              <div  className="row row-cols-1 row-cols-md-4 g-4">
-                              <div  style={{display:"none"}} id="divisionTrophy1" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture21.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 1</figcaption></figure>
+                              <div  className="row row-cols-1 row-cols-md-3 g-4">
+                              <div  style={{display:"none"}} id="divisionTrophy1" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture21.png")} /><figcaption className="text-center">Level 1</figcaption></figure>
                               </div>
-                              <div style={{display:"none"}} id="divisionTrophy2" className="col">
-                              <figure><img  style={{width:"100px",height:"100px"}}className="cardImg" variant="top" src={require("./assets/Picture22.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 2</figcaption></figure>
+                              <div style={{display:"none"}} id="divisionTrophy2" className="col text-center">
+                              <figure><img  style={{width:"100px",height:"100px"}}className="cardImg" variant="top" src={require("./assets/Picture22.png")} /><figcaption className="text-center">Level 2</figcaption></figure>
                               </div>
-                              <div  style={{display:"none"}} id="divisionTrophy3" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture23.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 3</figcaption></figure>
+                              <div  style={{display:"none"}} id="divisionTrophy3" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture23.png")} /><figcaption className="text-center">Level 3</figcaption></figure>
                                
                               </div>
-                              <div  style={{display:"none"}} id="divisionTrophy4" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture24.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 4</figcaption></figure>
+                              <div  style={{display:"none"}} id="divisionTrophy4" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture24.png")} /><figcaption className="text-center">Level 4</figcaption></figure>
                               </div>
-                              <div  style={{display:"none"}} id="divisionTrophy5" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture25.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 5</figcaption></figure>
+                              <div  style={{display:"none"}} id="divisionTrophy5" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture25.png")} /><figcaption className="text-center">Level 5</figcaption></figure>
                               </div>
-                              <div  style={{display:"none"}} id="divisionTrophy6" className="col">
-                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture26.png")} /><figcaption >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Level 6</figcaption></figure>
+                              <div  style={{display:"none"}} id="divisionTrophy6" className="col text-center">
+                              <figure><img style={{width:"100px",height:"100px"}} className="cardImg" variant="top" src={require("./assets/Picture26.png")} /><figcaption className="text-center">Level 6</figcaption></figure>
                               </div></div>
                              
                         </Card.Body>
